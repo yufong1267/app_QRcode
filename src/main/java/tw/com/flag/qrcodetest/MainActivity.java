6d8bba9306300c008fc838b1cb18fcf298480144
Berry <a2583669@gmail.com>
2017-11-11 16:19:34 +0800

    app/add/forward
    this time keep QRcode Scan and add
     some advance function in this app.
    This version can select table and keep data.

--- src/main/java/tw/com/flag/qrcodetest/MainActivity.java
+++ src/main/java/tw/com/flag/qrcodetest/MainActivity.java
@@ -1,11 +1,18 @@
 package tw.com.flag.qrcodetest;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -13,10 +20,30 @@ import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //not change display method
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+        //for always strangt display
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //always screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        //close title bar
+        getSupportActionBar().hide();
+        //close system bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //create database block
+
+        db = openOrCreateDatabase("testdb", Context.MODE_PRIVATE,null);
+        //create table the name of test
+        String createTable = "CREATE TABLE IF NOT EXISTS " + "test" + "(item VARCHAR(32))";
+        db.execSQL(createTable);
+
         final Activity activity = this;
-        button  = (Button)findViewById(R.id.scan_btn);
+//        button  = (Button)findViewById(R.id.scan_btn);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //
@@ -39,7 +66,10 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
     }
+    //QRcode method
     public void onClick(View v) {
+
+
         IntentIntegrator integrator = new IntentIntegrator(this);
         integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
         integrator.setPrompt("Scanning...");
@@ -58,10 +88,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
         if(result != null) {
             if(result.getContents() == null) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "cancelled", Toast.LENGTH_LONG).show();
             } else {
+                String log = result.getContents().toString();
+                if(log.equals("陳昕"))
+                {
+                    //嗆報小志志
+                    final TextView fuck = (TextView)findViewById(R.id.fuxk);
+
+                    fuck.setText("志剛小雞雞 吃宵夜搂~");
+                }
+//                adddata in table
+               addData(log);
+               Cursor c = db.rawQuery("SELECT * FROM test" , null);
+//               String output = c.getString(0);
+               final TextView print = (TextView)findViewById(R.id.out);
+                c.moveToFirst();
+                boolean check = false;
+
+
+                do {
+                    if(c.getString(0).equals("item333"))
+                    {
+                        check = true;
+                        break;
+                    }
+                }while(c.moveToNext());
+                if(check)
+                {
+                    print.setVisibility(View.VISIBLE);
+                }
+
                 Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
             }
         } else {
@@ -69,6 +129,16 @@ public class MainActivity extends AppCompatActivity {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
+
+//    build function of adding data
+    public void addData(String item)
+    {
+        ContentValues cv = new ContentValues(1);
+        cv.put("item" , item);
+
+        db.insert("test" , null , cv);
+    }
+
 //    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 //        IntentResult result = IntentIntegrator.parseActivityResult(requestCode , resultCode , data);
 //        if(result != null){
